1a1284f4ed2e6081548b1ff6ff67b2d87ff1a0ef
akemialice <akemialicetomioka@gmail.com>
2023-04-26 00:45:52 -0300

    Initial commit

---
+++ poc/src/main/java/decidimosdepois/poc/controller/ImagensController.java
@@ -0,0 +1,13 @@
+package decidimosdepois.poc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ImagensController {
+
+    @GetMapping("/imagens")
+    public String exibirImagens() {
+        return "html/paginaInicial.html";
+    }
+}
